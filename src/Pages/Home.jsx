1e26de5b42fe2978373526700725cc2d16020e69
posternaka <nik.petrenko97@gmail.com>
2022-10-06 16:26:40 +0300

    create pagination

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,31 +4,31 @@ import Categories from '@/components/Categories';
 import Sort from '@/components/Sort';
 import PizzaBlock from '@/components/PizzaBlock/PizzaBlock';
 import Skeleton from '@/components/PizzaBlock/PizzaPreloadPlaceholder';
+import Pagination from '@/components/Pagination/index';
 
 function Home({ searchValue }) {
     const [items, setItems] = React.useState([]);
     const [isLoading, setIsLoading] = React.useState(true);
-
+    const [currentPage, setCurrentPage] = React.useState(1);
     const [categoryId, setCategoryId] = React.useState(0);
     const [sortType, setSortType] = React.useState(
         {name: 'популярности ↑', sortType: 'rating', order: 'asc'},
     );
 
     const search = searchValue ? `&search=${searchValue}` : '';
-    console.log(search);
 
     React.useEffect(() => {
         const category = categoryId > 0 ? `category=${categoryId}` : '';
 
         setIsLoading(true)
-        fetch(`https://62dba18de56f6d82a774e889.mockapi.io/items?${category}${search}&sortBy=${sortType.sortType}&order=${sortType.order}`) 
+        fetch(`https://62dba18de56f6d82a774e889.mockapi.io/items?page=${currentPage}&limit=4&${category}${search}&sortBy=${sortType.sortType}&order=${sortType.order}`) 
         .then(res => res.json())
         .then(item => {
             setItems(item)
             setIsLoading(false)
         });
         window.scrollTo(0, 0);
-    }, [categoryId, sortType, searchValue])
+    }, [categoryId, sortType, searchValue, currentPage])
 
     return (
         <div className="container">
@@ -44,6 +44,7 @@ function Home({ searchValue }) {
                     : items.map(data => <PizzaBlock key={data.id} {...data} />)
                 }
             </div>
+            <Pagination onPageChange={(page) => setCurrentPage(page)} />
         </div>
     )
 }
---
+++ src/components/Pagination/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactPaginate from 'react-paginate';
+
+import styles from './Pagination.module.scss';
+
+function Pagination({onPageChange}) {
+  return (
+    <ReactPaginate
+        className={styles.root}
+        breakLabel="..."
+        nextLabel=">"
+        onPageChange={event => onPageChange(event.selected + 1)}
+        pageRangeDisplayed={8}
+        pageCount={3}
+        previousLabel="<"
+        renderOnZeroPageCount={null}
+      />
+  )
+}
+
+export default Pagination;
