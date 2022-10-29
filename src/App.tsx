ffcbac5a5d932a110fb25115f93982c8dc08bcb4
posternaka <nik.petrenko97@gmail.com>
2022-10-29 23:47:41 +0300

    start optimization with code splitting

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './styles/scss/app.scss';
 
 import Header from './components/Header';
+
 import Home from './Pages/Home';
-import NotFound from './Pages/NotFound';
-import Cart from './Pages/Cart';
-import FullCartPizza from './Pages/FullCartPizza';
+
+const Cart = React.lazy(() => import('./Pages/Cart'));
+const NotFound = React.lazy(() => import('./Pages/NotFound'));
+const FullCartPizza = React.lazy(() => import('./Pages/FullCartPizza'));
 
 
 function App() {
@@ -15,9 +18,21 @@ function App() {
             <div className="content">
                 <Routes>
                     <Route path="/" element={<Home />}  />
-                    <Route path="*" element={<NotFound />} />
-                    <Route path="/cart" element={<Cart />} />
-                    <Route path='/pizza/:id' element={<FullCartPizza />}/>
+                    <Route path="*" element={
+                        <React.Suspense fallback={<div>Идет загрузка данных...</div>}>
+                            <NotFound />
+                        </React.Suspense>} 
+                    />
+                    <Route path="/cart" element={
+                        <React.Suspense fallback={<div>Идет загрузка данных...</div>}>
+                            <Cart />
+                        </React.Suspense>} 
+                    />
+                    <Route path='/pizza/:id' element={
+                        <React.Suspense fallback={<div>Идет загрузка данных...</div>}>
+                            <FullCartPizza />
+                        </React.Suspense>}
+                    />
                 </Routes> 
             </div>
         </div>
